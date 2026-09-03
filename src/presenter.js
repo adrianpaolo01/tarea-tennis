b174b3f49e4fc5dea7505d0f14db21d3f4dbceca
Adrian Paolo <adrianpaolovalverde@gmail.com>
2026-09-03 09:38:58 -0400

    feat: front basico para visializar marcador

--- src/presenter.js
+++ src/presenter.js
@@ -1,9 +1,35 @@
 import sumar from "./sumador";
+import Tennis from "./tennis";
 
 const first = document.querySelector("#primer-numero");
 const second = document.querySelector("#segundo-numero");
 const form = document.querySelector("#sumar-form");
 const div = document.querySelector("#resultado-div");
+const score = document.querySelector("#tennis-score");
+const playerOneButton = document.querySelector("#jugador1-button");
+const playerTwoButton = document.querySelector("#jugador2-button");
+const resetButton = document.querySelector("#reiniciar-button");
+const tennis = new Tennis();
+
+const updateScore = () => {
+  score.textContent = tennis.obtenerScore();
+};
+
+playerOneButton.addEventListener("click", () => {
+  tennis.jugador1Anota();
+  updateScore();
+});
+
+playerTwoButton.addEventListener("click", () => {
+  tennis.jugador2Anota();
+  updateScore();
+});
+
+resetButton.addEventListener("click", () => {
+  tennis.puntosJ1 = 0;
+  tennis.puntosJ2 = 0;
+  updateScore();
+});
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -11,5 +37,5 @@ form.addEventListener("submit", (event) => {
   const firstNumber = Number.parseInt(first.value);
   const secondNumber = Number.parseInt(second.value);
 
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  div.textContent = sumar(firstNumber, secondNumber);
 });
