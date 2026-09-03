009f3d97ab5bf942fa5aa597caf0d9ba7459db50
Adrian Paolo <adrianpaolovalverde@gmail.com>
2026-09-03 09:03:08 -0400

    feat: añadiendo funcionalidad jugador 2 anota

--- src/tennis.js
+++ src/tennis.js
@@ -1,6 +1,29 @@
 class Tennis {
-    obtenerScore(){
-        return "Love - Love"
+    puntosJ1 = 0
+    puntosJ2 = 0
+    
+    jugador1Anota(puntos){
+        this.puntosJ1 += 1
     }
+
+    jugador2Anota(){
+        this.puntosJ2 += 1
+    }
+    
+    obtenerScore(){
+        let score = "Love - Love"
+        
+        if(this.puntosJ1 === 1 && this.puntosJ2 === 0){
+            score = "15-Love" 
+        }
+
+        if(this.puntosJ1 === 0 && this.puntosJ2 === 1){
+            score = "Love-15" 
+        }
+
+        
+        return score
+    }
+    
 } 
 export default Tennis;
--- src/tennis.spec.js
+++ src/tennis.spec.js
@@ -1,9 +1,23 @@
 import Tennis from "./tennis";
 
 describe("Tennis", () => {
-  it("deberia msotrar Love-Love", () => {
+  it("deberia mostrar Love-Love", () => {
     let tennis = new Tennis();
     expect(tennis.obtenerScore()).toEqual("Love - Love");
   });
+
+  it("si el jugador 1 anotó una vez, deberia mostrar 15-love", () => {
+    let tennis = new Tennis();
+    tennis.jugador1Anota();
+    expect(tennis.obtenerScore()).toEqual("15-Love");
+  });
+
+  it("si el jugador 2 anotó una vez, deberia mostrar love-15", () =>{
+    let tennis = new Tennis();
+    tennis.jugador2Anota();
+    expect(tennis.obtenerScore()).toEqual("Love-15");
+  })
+
+  
 });
 
